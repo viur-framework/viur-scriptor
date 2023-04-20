0d2eae49b90f5c24c6f6a53bc5be0302cf4aab20
KadirBalku <Kadir.Balku@gmail.com>
2023-04-20 18:29:38 +0200

    feat: Keypress on enter now triggers submit of data

--- src/components/Interaction/Input.vue
+++ src/components/Interaction/Input.vue
@@ -1,114 +1,111 @@
 <template>
+  <sl-card :class="'interaction'">
+    <div slot="header">
+      {{ props.title }}
+    </div>
 
-    <sl-card  :class="'interaction'">
-		<div class="interaction-img">
-			<img src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&sat=-100&bri=-5"
-			class="">
-		</div>
+    <p class="paragraph">
+      {{ props.text }}
+    </p>
 
+    <sl-alert
+      v-if="error.length > 0"
+      variant="danger"
+      open
+      class="error-message"
+    >
+      {{ error }}</sl-alert
+    >
 
+    <sl-input
+      v-if="props.type !== 'text'"
+      :type="props.type + (props.useTime ? 'time-local' : '')"
+      v-model="value"
+      :readonly="!render"
+      @keypress.enter="send"
+    ></sl-input>
+    <sl-textarea
+      v-else
+      class="label-on-left"
+      v-model="value"
+      :readonly="!render"
+      @keypress.enter="send"
+    ></sl-textarea>
 
-        <div slot="header">
-            {{ props.title }}
-        </div>
-
-		<p class="paragraph">
-			{{  props.text }}
-		</p>
-
-		<sl-alert v-if="error.length > 0" variant="danger" open class="error-message"> {{ error }}</sl-alert>
-
-
-		<sl-input v-if="props.type !== 'text'" :type="props.type + (props.useTime ? 'time-local' : '')" v-model="value" :readonly="!render"></sl-input>
-		<sl-textarea v-else class="label-on-left" v-model="value" :readonly="!render"></sl-textarea>
-
-        <div slot="footer">
-            <sl-button
-				   size="small"
-				   v-show="render"
-				   variant="success"
-				   @click="send"> {{ t("send") }} </sl-button>
-        </div>
-    </sl-card>
+    <div slot="footer">
+      <sl-button size="small" v-show="render" variant="success" @click="send">
+        {{ t("send") }}
+      </sl-button>
+    </div>
+  </sl-card>
 </template>
 
-
 <script setup lang="ts">
-export interface Props {
-    title: String,
-    text: String,
-    type: String,
-    select: Function,
-    empty: Boolean,
-    useTime: Boolean
-}
+  export interface Props {
+    title: String;
+    text: String;
+    type: String;
+    select: Function;
+    empty: Boolean;
+    useTime: Boolean;
+  }
 
+  import { ref, watch } from "vue";
+  import { useI18n } from "vue-i18n";
 
-import {ref, watch} from 'vue';
-import { useI18n } from 'vue-i18n';
+  const render = ref(true);
+  const { t } = useI18n();
+  const selectedValue = ref<boolean>(undefined);
+  const value = ref<String>("");
 
-const render = ref(true);
-const {t} = useI18n();
-const selectedValue = ref<boolean>(undefined);
-const value = ref<String>("");
+  const error = ref<String>("");
 
-const error = ref<String>("");
+  const props = defineProps<Props>();
 
-
-const props = defineProps<Props>();
-
-watch(value, (oldValue, newValue) => {
+  watch(value, (oldValue, newValue) => {
     if (value.value) {
-        error.value = "";
+      error.value = "";
     }
-});
+  });
 
-function send() {
-    if (!render.value)
-        return;
+  function send() {
+    if (!render.value) return;
 
     if (!props.empty) {
-        if (!value.value) {
-            error.value = t("error.empty");
-            return;
-        }
+      if (!value.value) {
+        error.value = t("error.empty");
+        return;
+      }
     }
 
     let tmpValue = value.value;
     if (props.type === "date") {
-        tmpValue = new Date(value.value).valueOf();
+      tmpValue = new Date(value.value).valueOf();
     }
 
     props.select(tmpValue);
     render.value = false;
-
-}
-
-
+  }
 </script>
 
-
 <style scoped lang="less">
+  .interaction-img {
+    margin: -10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: calc(100% + 20px);
+    height: 200px;
+    margin-bottom: 20px;
+    background-color: var(--sl-color-neutral-100);
 
-
-.interaction-img{
-	margin: -10px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: calc(100% + 20px);
-	height: 200px;
-	margin-bottom: 20px;
-	background-color: var(--sl-color-neutral-100);
-
-	img{
-	  object-fit: contain;
-	  height: 100%;
-	}
+    img {
+      object-fit: contain;
+      height: 100%;
+    }
   }
 
-.error-message {
-   margin-bottom: 15px;
-}
-
+  .error-message {
+    margin-bottom: 15px;
+  }
 </style>
